ce5f26640800014ac4817bc3fdf06ce38de32741
Billy Hung <billy7907@gmail.com>
2017-06-13 15:18:49 -0700

    added comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
+// using axios library to handle get request
 import axios from 'axios';
+
 const API_KEY = 'b38e9999c29b2824af0f489d3251d284';
+// documentation from openweathermap.org
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
