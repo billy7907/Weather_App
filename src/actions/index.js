8c7ffe282123d5d0819d5ce5c3c96a22e021ae84
Billy Hung <billy7907@gmail.com>
2017-06-13 15:17:30 -0700

    added axios library

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,15 @@
-
+import axios from 'axios';
 const API_KEY = 'b38e9999c29b2824af0f489d3251d284';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    paylaod: request
+  };
+}
