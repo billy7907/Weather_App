665b2da933f22fa941aa12750079e9acb88f6d2c
Billy Hung <billy7907@gmail.com>
2017-06-14 15:48:15 -0700

    Added notes on state manipulation

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,18 @@
-export default function(state=null, action){
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action){
+
+  switch(action.type){
+  case FETCH_WEATHER:
+    /* In Redux, we never want to manipulate the state, which we would've done if we use
+       state.push(action.payload.data). Instead, we want to create a new array and
+       return that instead by using concat. 
+    */
+
+    // return state.concat([action.payload.data]);
+    // 👇 is the same as 👆
+    return [action.payload.data, ...state];
+  }
 
   return state;
 }
