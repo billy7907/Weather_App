d45dd5680a2b1afbfd7b6280c32880150646a023
Billy Hung <billy7907@gmail.com>
2017-06-15 13:26:46 -0700

    fixed css format

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,7 +7,7 @@ class WeatherList extends Component {
   renderWeather(cityData){
     const name = cityData.city.name;
     // converting to Celsius
-    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp-273);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
 
