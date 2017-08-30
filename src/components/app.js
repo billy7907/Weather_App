7e5ac04dee1b1558ab03f693af7d607ab6f01459
Billy Hung <billy7907@gmail.com>
2017-08-30 12:47:15 -0700

    changed style

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {Component} from 'react';
+import { Component } from 'react';
 import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <SearchBar />
-        <WeatherList />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<br />
+				<h1>My Weather App</h1>
+				<SearchBar />
+				<WeatherList />
+			</div>
+		);
+	}
 }
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,52 +1,62 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Chart from '../components/chart';
 import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
+	renderWeather(cityData) {
+		const name = cityData.city.name;
+		// converting to Celsius
+		const temps = _.map(
+			cityData.list.map(weather => weather.main.temp),
+			temp => temp - 273
+		);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const { lon, lat } = cityData.city.coord;
+		// 👆 same as 👇
+		// const lon = cityData.city.coord.lon;
+		// const lat = cityData.city.coord.lat;
 
-  renderWeather(cityData){
-    const name = cityData.city.name;
-    // converting to Celsius
-    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
-    const pressures = cityData.list.map(weather => weather.main.pressure);
-    const humidities = cityData.list.map(weather => weather.main.humidity);
-    const { lon, lat } = cityData.city.coord;
-    // 👆 same as 👇
-    // const lon = cityData.city.coord.lon;
-    // const lat = cityData.city.coord.lat;
+		return (
+			<tr key={name}>
+				<td>
+					<GoogleMap lon={lon} lat={lat} />
+				</td>
+				<td>
+					<Chart data={temps} color="red" units="˚C" />
+				</td>
+				<td>
+					<Chart data={pressures} color="black" units="hPA" />
+				</td>
+				<td>
+					<Chart data={humidities} color="blue" units="%" />
+				</td>
+			</tr>
+		);
+	}
 
-    return(
-      <tr key={name}>
-        <td><GoogleMap lon={lon} lat={lat} /></td>
-        <td><Chart data={temps} color='red' units='˚C' /></td>
-        <td><Chart data={pressures} color='black' units='hPA' /></td>
-        <td><Chart data={humidities} color='blue' units='%' /></td>
-      </tr>
-    );
-  }
-
-  render(){
-    return(
-      <table className='table table-hover'>
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature (˚C)</th>
-            <th>Pressure (hPA)</th>
-            <th>Humidity (%)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.weather.map(this.renderWeather)}
-        </tbody>
-      </table>
-    );
-  }
+	render() {
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>City</th>
+						<th>Temperature (˚C)</th>
+						<th>Pressure (hPA)</th>
+						<th>Humidity (%)</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.props.weather.map(this.renderWeather)}
+				</tbody>
+			</table>
+		);
+	}
 }
 
-function mapsStateToProps({weather}){
-  return {weather};
+function mapsStateToProps({ weather }) {
+	return { weather };
 }
 
 export default connect(mapsStateToProps)(WeatherList);
